72f7329f1ecbd2837d6532b843534bc208a3f6f0
idi-moe <70919760+idi-moe@users.noreply.github.com>
2021-10-21 20:59:59 -0400

    little endian or something like that

--- readbit.m
+++ readbit.m
@@ -1,7 +1,7 @@
 function [bitout] = readbit(byte,bit)
 %READBIT reads a bit from a given byte (int), bit is index'd at 0
 %   Detailed explanation goes here
-bits = logical(bitand(byte,[1,2,4,8,16,32,64,128]));
+bits = logical(bitand(byte,[128,64,32,16,8,4,2,1]));
 bit=bit+1;
 bitout = bits(bit);
 end
