920943a121adfcfa081147ed53a6cc85de9b085c
idi-moe <70919760+idi-moe@users.noreply.github.com>
2021-10-21 20:59:14 -0400

    now it works, with indexing at 0 as it should

--- psw.m
+++ psw.m
@@ -1,7 +1,45 @@
-function [outputArg1,outputArg2] = psw(inputArg1,inputArg2)
-%PSW Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [data] = psw(dir,flag,value)
+%PSW R/W access to the program status word register (PSW)
+%   Located at 0xD0, this register holds the 'cy' carry flag, 'ac' aux
+%   carry flag (for bcd), 'f0' general purpose user flag 0, 'rs1' & 'rs0'
+%   register bank select bits, 'ov' overflow flag, 'f1' general purpose
+%   user flag, and 'p' parity flag, which is set/cleared each instr cycle
+%   to indicate and odd/even # of one bits in the acc, i.e. even parity.
+%   See page 25 of the Siemens SAB80C515 user manual. The layout is
+%   cy,ac,f0,rs1,rs0,ov,f1,p from 0xD7 to 0xD0.
+
+psw = dba(0xD0);
+bit = [];
+
+switch flag %remember that the lower level r/w functions index at 0!
+    case 'cy'
+        bit = 7;
+    case 'ac'
+        bit = 6;
+    case 'f0'
+        bit = 5;
+    case 'rs1'
+        bit = 4;
+    case 'rs0'
+        bit = 3;
+    case 'ov'
+        bit = 2;
+    case 'f1'
+        bit = 1;
+    case 'p'
+        bit = 0;
+end
+        
+
+if dir == 'r'
+    data = readbit(psw,bit);
+end
+
+if dir == 'w'
+    psw = writebit(psw,bit,value);
+    dba(0xD0,'w',psw)
+    data = 0;
+end
+    
 end
 
