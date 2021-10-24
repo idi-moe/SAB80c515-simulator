995961a22407374c5b257ef52b00e0d4df653e45
idi-moe <70919760+idi-moe@users.noreply.github.com>
2021-10-23 20:47:25 -0400

    Add files via upload

---
+++ instructions/NOP00.m
@@ -0,0 +1,8 @@
+function [] = NOP00()
+%NOP00 NOP, No Operation
+%   Execution continuesat the following instruction. Other than the PC, no 
+%   registersor flags are affected. Used for timing purposes.
+%   (PC) <- (OC) + 1
+
+end
+
