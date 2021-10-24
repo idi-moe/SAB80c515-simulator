decae0f02823b78ce0dcda024e4c1607591e3fe4
idi-moe <70919760+idi-moe@users.noreply.github.com>
2021-10-23 22:47:56 -0400

    oscillator period 12

--- instructions/NOP00.m
+++ instructions/NOP00.m
@@ -3,6 +3,7 @@ function [] = NOP00()
 %   Execution continuesat the following instruction. Other than the PC, no 
 %   registersor flags are affected. Used for timing purposes.
 %   (PC) <- (OC) + 1
+% Oscillator period 12
 
 end
 
