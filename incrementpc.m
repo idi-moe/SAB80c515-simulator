708f9bceceac9025785da2ba05ad9588a12cd8cb
idi-moe <70919760+idi-moe@users.noreply.github.com>
2021-11-01 20:30:48 -0400

    Add files via upload

---
+++ incrementpc.m
@@ -0,0 +1,8 @@
+function [] = incrementpc()
+%INCREMENTPC Increments the program counter (PC)
+%   The PC is not addressable, and evry instruction increments it. Some
+%   modify it as well, and some read it.
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
