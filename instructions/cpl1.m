eedd6311a4d770477a33ce4a18d6f232efa04eab
idi-moe <70919760+idi-moe@users.noreply.github.com>
2021-11-03 19:24:46 -0400

    added 0xb3

--- instructions/cpl1.m
+++ instructions/cpl1.m
@@ -1,18 +1,26 @@
 function [] = cpl1(opcode)
-%CPL1 one's complement acc
-%   opcode 0xF4
+%CPL1 one's complement
+%   opcode 0xF4 - acc, opcode 0xB3 - c/cy flag
 
 acc = uint8(dba(0xE0));
 
-if opcode ~= 0xF4
+if (opcode ~= 0xF4) && (opcode ~= 0xB3)
     error('wrong opcode in cpl function')
 end
 
-accbits = logical(bitand(acc,uint8([1,2,4,8,16,32,64,128])));
-accbits = ~accbits;
-acc = sum(accbits .* [1,2,4,8,16,32,64,128]);
+if opcode == 0xF4
+    accbits = logical(bitand(acc,uint8([1,2,4,8,16,32,64,128])));
+    accbits = ~accbits;
+    acc = sum(accbits .* [1,2,4,8,16,32,64,128]);
+    
+    dba(0xE0,'w',acc);
+end
 
-dba(0xE0,'w',acc);
+if opcode == 0xB3
+    c = psw('cy');
+    c = ~c;
+    psw('cy','w',c);
+end
 
 incrementpc();
 
